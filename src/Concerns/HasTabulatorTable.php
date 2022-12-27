dee87608c5d53b97c1a2a55b7dff8eae2e224725
Victor R <39545521+viicslen@users.noreply.github.com>
2022-12-27 15:54:59 -0500

    Add concern for models to easily define tables

---
+++ src/Concerns/HasTabulatorTable.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace FmTod\LaravelTabulator\Concerns;
+
+use FmTod\LaravelTabulator\Contracts\RendersTable;
+use FmTod\LaravelTabulator\TabulatorTable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+trait HasTabulatorTable
+{
+    abstract public static function tabulatorClass(): string;
+
+    public static function tabulatorTable(?Request $request = null): TabulatorTable
+    {
+        return app(self::tabulatorClass(), $request ? [$request] : []);
+    }
+
+    public static function tabulatorRender(string $view, array $data = [], RendersTable|string|null $renderer = null): mixed
+    {
+        return self::tabulatorTable()->render($view, $data, $renderer);
+    }
+
+    public static function tabulatorRules(string $arrayKey = null, string $keyName = null): array
+    {
+        /** @var \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Builder $model */
+        $model = self::newModelInstance();
+        $table = $model->getTable();
+
+        if (! $keyName) {
+            $keyName = $model->getKeyName();
+        }
+
+        if (! $arrayKey) {
+            $arrayKey = Str::singular($table).'_'.Str::plural($keyName);
+        }
+
+        return [
+            'type' => ['required', 'string', 'in:array,query'],
+
+            $arrayKey => ['prohibited_if:type,query', 'array'],
+            "$arrayKey.*" => ['prohibited_if:type,query', 'integer', "exists:$table,$keyName"],
+
+            'filters' => ['prohibited_if:type,array', 'array'],
+            'sort' => ['prohibited_if:type,array', 'array'],
+        ];
+    }
+
+    public static function tabulatorQuery(Request $request, string $arrayKey = null, string $keyName = null): Builder
+    {
+        /** @var \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Builder $model */
+        $model = self::newModelInstance();
+        $table = self::tabulatorTable($request);
+
+        if (! $keyName) {
+            $keyName = $model->getKeyName();
+        }
+
+        if (! $arrayKey) {
+            $arrayKey = Str::singular($model->getTable()).'_'.Str::plural($keyName);
+        }
+
+        $validated = $request->validate(self::tabulatorRules($keyName, $arrayKey));
+
+        /** @var \Illuminate\Database\Eloquent\Builder $query */
+        $query = $validated['type'] === 'array'
+            ? $model->whereIn($keyName, $validated[$arrayKey])
+            : $table->getScopedQuery();
+
+        return $query;
+    }
+}
